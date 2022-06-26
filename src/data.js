a9b6e2b9edc5c02c9abe69fbce38982c72a4bdfc
madda01 <madhuminipgunaratne@gmail.com>
2022-06-26 13:50:03 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data ={
+    products:[
+        {
+            id: '1',
+            name: 'Laptop',
+            price: 440000,
+            image: 'https://picsum.photos/id/180/2400/1600',
+          },
+          {
+            id: '2',
+            name: 'Old Car',
+            price: 2400000,
+            image: 'https://picsum.photos/id/111/4400/2656',
+          },
+          {
+            id: '3',
+            name: 'Women Shoes',
+            price: 5000,
+            image: 'https://picsum.photos/id/21/3008/2008',
+          },
+    ],
+};
+
+export default data;
